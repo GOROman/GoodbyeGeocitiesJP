430026b6295954f317ab23b8a5eed09a15402226
GOROman <goro@net-kick.com>
2019-05-08 01:23:19 +0900

    Initial commit.

---
+++ src/jump.js
@@ -0,0 +1,35 @@
+var redirectUrl = document.getElementsByClassName('userpageRedirectUrl');
+
+if(redirectUrl.length)
+{
+    var userpageRedirectTitle = document.getElementsByClassName('userpageRedirectTitle');
+    setInterval(function () {
+        if(userpageRedirectTitle){
+            userpageRedirectTitle[0].textContent = '移転先へ自動へ飛びます！';
+        }
+        setInterval(function () {
+            var url = redirectUrl[0].textContent;
+            // URL書き換える
+            location.replace(url);
+        },1000);
+    },1000);
+} else {
+
+    var errorTtlTxt = document.getElementsByClassName('errorTtlTxt');
+    if (errorTtlTxt.length) {
+        setInterval(function () {
+            var url = "https://web.archive.org/web"+location.href;
+
+            errorTtlTxt[0].textContent = 'Web Achiveに自動で飛ぶぜ！！';
+            var errorInfoTxt = document.getElementsByClassName('errorInfoTxt');
+            if (errorInfoTxt) {
+                errorInfoTxt[0].textContent = url;
+            }
+
+            setInterval(function () {    
+                // URL書き換える
+                location.replace(url);
+            },1000);
+        },1000);
+    }
+}
